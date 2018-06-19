2f0f83e4a74c7260c575d3455f801799b8fdf657
Mounir El Kirafi <mounir9912@gmail.com>
2018-06-20 01:36:27 +0200

    OpenCV working now. Experimented with camera display, it's working now, though it is streched.

--- ARSS/app/src/main/java/com/example/uva/arss/MainActivity.java
+++ ARSS/app/src/main/java/com/example/uva/arss/MainActivity.java
@@ -1,13 +1,118 @@
 package com.example.uva.arss;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    CameraBridgeViewBase camera;
+    Mat mat, matF, matT;
+    BaseLoaderCallback baseLoaderCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPermission();
+
+        if(OpenCVLoader.initDebug()) {
+            Toast.makeText(getApplicationContext(), "loaded", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "not loaded", Toast.LENGTH_SHORT).show();
+        }
+
+        camera = (JavaCameraView) findViewById(R.id.myCameraView);
+        camera.setVisibility(SurfaceView.VISIBLE);
+        camera.setCvCameraViewListener(this);
+
+        baseLoaderCallback = new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+                switch (status) {
+                    case BaseLoaderCallback.SUCCESS:
+                        camera.enableView();
+                        break;
+                    default:
+                        super.onManagerConnected(status);
+                        break;
+
+                }
+            }
+        };
     }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mat = inputFrame.rgba();
+
+        Core.transpose(mat, matT);
+        Imgproc.resize(matT, matF, matF.size(), 0, 0, 0);
+        Core.flip(matF, mat, 1);
+        
+        return mat;
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mat = new Mat(height, width, CvType.CV_8UC4);
+        matF = new Mat(height, width, CvType.CV_8UC4);
+        matT = new Mat(width, width, CvType.CV_8UC4);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mat.release();
+//        matF.release();
+//        matT.release();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(camera != null) {
+            camera.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!OpenCVLoader.initDebug()) {
+            Toast.makeText(getApplicationContext(), "Opencv problem", Toast.LENGTH_LONG).show();
+        }
+        else {
+            baseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(camera != null) {
+            camera.disableView();
+        }
+    }
+
+    private void checkPermission(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+        }
+    }
+
 }
