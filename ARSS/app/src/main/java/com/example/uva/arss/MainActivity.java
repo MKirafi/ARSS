ca57136e184f2bf8867fa43d32ffea05b26e4657
Abdelilah Ahbari <AbdelilahAhbari@gmail.com>
2018-06-20 10:52:21 +0200

    Sudoku bruteforce solver

--- ARSS/app/src/main/java/com/example/uva/arss/MainActivity.java
+++ ARSS/app/src/main/java/com/example/uva/arss/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.uva.arss;
+import com.example.uva.arss.Sudoku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,5 +10,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int[][] grid2 = {
+                {0,0,7,0,6,0,1,0,9},
+                {0,9,0,2,7,0,3,0,0},
+                {0,0,0,0,0,5,0,0,7},
+                {0,0,9,7,0,0,5,0,3},
+                {7,0,0,0,0,0,0,0,8},
+                {5,0,1,0,0,9,6,0,0},
+                {2,0,0,9,0,0,0,0,0},
+                {0,0,4,0,5,7,0,2,0},
+                {3,0,8,0,4,0,7,0,0}
+        };
+
+        int[][] grid = {
+                {8,0,0,0,0,0,0,0,0},
+                {0,0,3,6,0,0,0,0,0},
+                {0,7,0,0,9,0,2,0,0},
+                {0,5,0,0,0,7,0,0,0},
+                {0,0,0,0,4,5,7,0,0},
+                {0,0,0,1,0,0,0,3,0},
+                {0,0,1,0,0,0,0,6,8},
+                {0,0,8,5,0,0,0,1,0},
+                {0,9,0,0,0,0,4,0,0}
+        };
+
+        Sudoku sudoku = new Sudoku(grid);
+        int[][] newGrid = sudoku.solveSudoku(grid);
+        for(int i = 0; i < grid[0].length; i++){
+            System.out.print("{");
+            for(int j = 0; j < grid.length; j++){
+                if(j == 3 || j == 6){
+                    System.out.print("|");
+                }
+                System.out.print(grid[i][j] + ", ");
+            }
+            System.out.println("}");
+        }
+
     }
 }
