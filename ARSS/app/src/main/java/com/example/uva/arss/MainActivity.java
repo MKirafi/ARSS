4bfa5de72ebf80a4c6e68e0c591630a7e57c44bf
Mund Vetter <mundvetter@gmail.com>
2018-06-21 13:18:09 +0200

    added speech regocnition

--- ARSS/app/src/main/java/com/example/uva/arss/MainActivity.java
+++ ARSS/app/src/main/java/com/example/uva/arss/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.uva.arss;
 
+import android.content.ActivityNotFoundException;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private IntentManager intentManager;
+    private TextView txtOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.txtOutput = findViewById(R.id.txt_output);
+        this.intentManager = new IntentManager();
+
+        /* "record audio" button: */
+        findViewById(R.id.record_audio).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    startActivityForResult(intentManager.intent, 1);
+                } catch (ActivityNotFoundException a) {
+                    Toast.makeText(getApplicationContext(),
+                            "No speech. No gain. Pls buy a new  phone.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    /**
+     * Callback for speech recognition activity
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String text = result.get(0);
+                    txtOutput.setText(text);
+                }
+                break;
+            }
+        }
     }
 }
