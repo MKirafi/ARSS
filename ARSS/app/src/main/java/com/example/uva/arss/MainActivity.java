1c96479c0532aff2a82b589fa35319213b0ff809
Mounir El Kirafi <mounir9912@gmail.com>
2018-06-21 13:54:14 +0200

    Sudoku grabber WIP. Image preprocessing applied and contouring WIP.

--- ARSS/app/src/main/java/com/example/uva/arss/MainActivity.java
+++ ARSS/app/src/main/java/com/example/uva/arss/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.uva.arss;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Picture;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,15 +14,28 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.photo.Photo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.opencv.core.CvType.CV_8UC1;
+import static org.opencv.core.CvType.CV_8UC4;
+import static org.opencv.imgproc.Imgproc.CHAIN_APPROX_SIMPLE;
+import static org.opencv.imgproc.Imgproc.RETR_EXTERNAL;
+import static org.opencv.imgproc.Imgproc.RETR_LIST;
+import static org.opencv.imgproc.Imgproc.contourArea;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     CameraBridgeViewBase camera;
-    Mat mat, matF, matT;
+    Mat mat, matF, matT, hierarchy;
     BaseLoaderCallback baseLoaderCallback;
+    List<MatOfPoint> contours = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,26 +74,45 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         mat = inputFrame.rgba();
-
+        hierarchy = new Mat();
         Core.transpose(mat, matT);
         Imgproc.resize(matT, matF, matF.size(), 0, 0, 0);
         Core.flip(matF, mat, 1);
+          Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGBA2GRAY);
+          Imgproc.GaussianBlur(mat, mat, new Size(11, 11), 0);
+          Imgproc.adaptiveThreshold(mat, mat, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 5, 2);
+          Core.bitwise_not(mat, mat);
+          Imgproc.Canny(mat, mat, 400, 600, 5, true);
+          Imgproc.findContours(mat.clone(), contours, hierarchy,RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 
+          double largest_area = 0;
+          int largest_area_index = 0;
+          Mat largest_contour = new Mat();
+          for(int i = 0; i < contours.size(); i++) {
+              double size = contourArea(contours.get(i), false);
+            if(size > largest_area) {
+                largest_area = size;
+                largest_area_index = i;
+                largest_contour = contours.get(i);
+            }
+          }
+          System.out.println("Rows: " + largest_contour.rows());
+          System.out.println("Columns: " + largest_contour.cols());
         return mat;
     }
 
     @Override
     public void onCameraViewStarted(int width, int height) {
-        mat = new Mat(height, width, CvType.CV_8UC4);
-        matF = new Mat(height, width, CvType.CV_8UC4);
-        matT = new Mat(width, width, CvType.CV_8UC4);
+        mat = new Mat(height, width, CV_8UC4);
+        matF = new Mat(height, width, CV_8UC4);
+        matT = new Mat(width, width, CV_8UC4);
     }
 
     @Override
     public void onCameraViewStopped() {
         mat.release();
-//        matF.release();
-//        matT.release();
+        matF.release();
+        matT.release();
     }
 
     @Override
