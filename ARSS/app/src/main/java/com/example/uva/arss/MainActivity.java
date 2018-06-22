19c62c8e30936ea15257cf22d74b07dadbc384f6
Abdelilah Ahbari <AbdelilahAhbari@gmail.com>
2018-06-22 11:42:14 +0200

    added new logo

--- ARSS/app/src/main/java/com/example/uva/arss/MainActivity.java
+++ ARSS/app/src/main/java/com/example/uva/arss/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.uva.arss;
-import com.example.uva.arss.Sudoku;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -38,43 +37,32 @@ public class MainActivity extends AppCompatActivity {
         };
 
         int[] sud = {8,0,0,0,0,0,0,0,0,
-                0,0,3,6,0,0,0,0,0,
-                0,7,0,0,9,0,2,0,0,
-                0,5,0,0,0,7,0,0,0,
-                0,0,0,0,4,5,7,0,0,
-                0,0,0,1,0,0,0,3,0,
-                0,0,1,0,0,0,0,6,8,
-                0,0,8,5,0,0,0,1,0,
-                0,9,0,0,0,0,4,0,0};
+                     0,0,3,6,0,0,0,0,0,
+                     0,7,0,0,9,0,2,0,0,
+                     0,5,0,0,0,7,0,0,0,
+                     0,0,0,0,4,5,7,0,0,
+                     0,0,0,1,0,0,0,3,0,
+                     0,0,1,0,0,0,0,6,8,
+                     0,0,8,5,0,0,0,1,0,
+                     0,9,0,0,0,0,4,0,0};
 
-        BruteSudoku bruteSudoku = new BruteSudoku(grid);
+    BruteSudoku bruteSudoku = new BruteSudoku(grid);
 
-        long startTime = System.currentTimeMillis();
-        //int[][] newGrid = bruteSudoku.solveSudoku(grid);
-        Sudoku.solve(sud, 0);
-        long stopTime = System.currentTimeMillis();
-        long elapsedTime = stopTime - startTime;
-        System.out.println(elapsedTime);
-        print(sud);
-
-
-//        for(int i = 0; i < grid[0].length; i++){
-//            System.out.print("{");
-//            for(int j = 0; j < grid.length; j++){
-//                if(j == 3 || j == 6){
-//                    System.out.print("|");
-//                }
-//                System.out.print(grid[i][j] + ", ");
-//            }
-//            System.out.println("}");
-//        }
+    long startTime = System.currentTimeMillis();
+//        int[][] newGrid = bruteSudoku.solveSudoku(grid);
+    Sudoku.solve(sud, 0);
+    long stopTime = System.currentTimeMillis();
+    long elapsedTime = stopTime - startTime;
+    System.out.println(elapsedTime);
+    print(sud);
 
     }
 
-    public static void print(int[] grid)
-    {
-        for(int i = 0; i < 81; i += 9)
+    public static void print(int[] grid) {
+        for(int i = 0; i < 81; i += 9){
             System.out.println(Arrays.toString(Arrays.copyOfRange(grid, i, i + 9)));
+        }
         System.out.println();
     }
 }
+
