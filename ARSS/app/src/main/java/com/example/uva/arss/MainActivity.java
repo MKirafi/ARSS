1fc3dc14e0d15793697cfc582bc018e0cc3202e5
Liam Zuiderhoek <liamzuid@hotmail.com>
2018-06-22 10:11:29 +0200

    The sudoku grid has been drawn but still has overflow issues when fully filled in, the camera can be opened by the take image button but crashes when a picture is being made.

--- ARSS/app/src/main/java/com/example/uva/arss/MainActivity.java
+++ ARSS/app/src/main/java/com/example/uva/arss/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.uva.arss;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.w3c.dom.*;
 
 public class MainActivity extends AppCompatActivity {
+        Document doc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button takeImage = findViewById(R.id.take_image_button);
+        takeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivity(intent);
+            }
+        });
     }
 }
